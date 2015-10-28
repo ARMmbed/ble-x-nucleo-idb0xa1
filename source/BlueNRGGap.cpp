e0f8ac8214bfabfff8690cd746fce1c6a2d6a2ce
Wolfgang Betz <wolfgang.betz@st.com>
2015-10-28 08:04:25 +0100

    Minor modification

--- source/BlueNRGGap.cpp
+++ source/BlueNRGGap.cpp
@@ -1021,17 +1021,14 @@ void BlueNRGGap::Discovery_CB(Reason_t reason,
     PRINTF("DISCOVERY_COMPLETE\n\r");
     _scanning = false;
 
-    // We need to wait for a while before creating a connection due to
-    // BlueNRG process queue handling
+    // FIXME: We need to wait for a while before creating a connection
+    // due to BlueNRG process queue handling
     Clock_Wait(100);
 
     if(_connecting) {
       makeConnection();  
     } else {
       PRINTF("re-startRadioScan\n\r");
-      // FIXME: We need to wait for a while before creating a connection
-      // due to BlueNRG process queue handling
-      Clock_Wait(100);
       startRadioScan(_scanningParams);
     }
 
